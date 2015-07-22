52c19b78e1e296f6ba1f1254235f2c55b6a7d333
NSSONGMENG <740621245@qq.com>
2015-07-22 22:51:06 +0800

    SegmentControl

---
+++ SegmentControl/SegmentControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SegmentControl
+//
+//  Created by songmeng on 15/7/7.
+//  Copyright (c) 2015年 songmeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
