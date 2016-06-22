8b66c8ed0781f7cc637fa0b227e5493c151e0e34
NSSONGMENG <740621245@qq.com>
2016-06-22 23:24:34 +0800

    添加新开源库以及图片

--- Pop/Pop/ViewController.m
+++ Pop/Pop/ViewController.m
@@ -8,15 +8,65 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
 
+@interface ViewController ()<UITableViewDataSource,UITableViewDelegate>
+@property (nonatomic, strong) NSArray   * imgArr;
+@property (nonatomic, strong) UITableView   * tableView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor greenColor];
+    _imgArr = @[[UIImage imageNamed:@"earth.jpg"],
+                [UIImage imageNamed:@"water.jpg"],
+                [UIImage imageNamed:@"moutain.jpg"],
+                [UIImage imageNamed:@"jobs.jpg"]];
+    
+    _tableView = [UITableView new];
+    _tableView.dataSource = self;
+    _tableView.delegate = self;
+    _tableView.separatorColor = [UIColor blackColor];
+    [self.view addSubview:_tableView];
+    [_tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.bottom.equalTo(self.view);
+        make.top.equalTo(self.view).offset(20);
+    }];
+}
+#pragma  mark  -
+#pragma  mark  --------- table view ---------
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [_imgArr count];
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 100;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString * cell_ident = @"cell_ident";
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:cell_ident];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cell_ident];
+    }
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"row : %ld",indexPath.row];
+    
+    cell.backgroundColor = [UIColor yellowColor];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    NSString * message = [NSString stringWithFormat:@" row : %ld", indexPath.row];
+    MTStatusBarOverlay *overlay = [MTStatusBarOverlay sharedInstance];
+    overlay.backgroundView.backgroundColor = [UIColor clearColor];
+    overlay.animation = MTStatusBarOverlayAnimationShrink;
+    overlay.detailViewMode = MTDetailViewModeHistory;
+    [overlay postImmediateFinishMessage:message duration:2.0 animated:YES];
+    overlay.progress = 0.5;
 }
 
 - (void)didReceiveMemoryWarning {
