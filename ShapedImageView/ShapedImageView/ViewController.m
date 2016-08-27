4b98e0460d2d0a03b7561046ba1c77627b0ea806
NSSONGMENG <740621245@qq.com>
2016-08-27 23:47:34 +0800

    替换图片，修改说明

--- ShapedImageView/ShapedImageView/ViewController.m
+++ ShapedImageView/ShapedImageView/ViewController.m
@@ -27,8 +27,8 @@
     [imgView0 mas_makeConstraints:^(MASConstraintMaker *make) {
         make.right.equalTo(self.view).offset(-20);
         make.top.equalTo(self.view).offset(30);
-        make.width.mas_equalTo(130);
-        make.height.mas_equalTo(@260);
+        make.width.mas_equalTo(200);
+        make.height.mas_equalTo(@320);
     }];
     
     
@@ -37,13 +37,13 @@
     [imgview1 mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(imgView0.mas_bottom).offset(20);
         make.left.equalTo(self.view).offset(20);
-        make.width.equalTo(@260);
-        make.height.mas_equalTo(130);
+        make.width.equalTo(@255);
+        make.height.mas_equalTo(165);
         //这里用equal和mas_equal是一样的，主要目的就是为了让不了解Masonry的朋友能熟悉这两种实质上相同的设置方式
         //当然这里的@200是一种语法糖，和[NSNumber numberWithInteger:200]的意思一致
         //其实还有一种比较鲜为人知的语法糖如下：
-        NSInteger   inte = 50;
-        NSNumber    * num = @(inte);
+//        NSInteger   inte = 50;
+//        NSNumber    * num = @(inte);
         //其实这里@(基本类型变量)也是一种NSNumber语法糖
     }];
     
